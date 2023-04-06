03b7fef0fc94bf033788bb42c019497117dfae02
Erkanat <tashenov05@gmail.com>
2023-04-07 00:26:08 +0600

    max example

---
+++ src/Experience/maxFunction.java
@@ -0,0 +1,15 @@
+package Experience;
+
+public class maxFunction {
+    public static void max(int a, int b) {
+        if(a>b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+
+    public static void main(String[] args) {
+        max(2, 3);
+    }
+}
